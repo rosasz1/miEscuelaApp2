5e13a53827c670aac57940462752384549ede7c0
valencastaldi <tinocastaldi04@gmail.com>
2025-07-03 12:45:07 -0300

    03/07

--- subir_git.bat
+++ subir_git.bat
@@ -1,15 +1,25 @@
 @echo off
 echo ===========================
-echo   SUBIDA A GITHUB - ProyectoEscuelas
+echo    SUBIDA A GITHUB
 echo ===========================
 
+REM Activar entorno virtual
+call .venv\Scripts\activate
+
+REM Mostrar rama actual
+for /f "delims=" %%i in ('git branch --show-current') do set branch=%%i
+echo Rama actual: %branch%
+
+REM Pedir mensaje de commit
 set /p msg=📝 Escribí tu mensaje de commit:
 echo.
 
+REM Ejecutar comandos Git
 git add .
 git commit -m "%msg%"
-git push origin main
+git push origin %branch%
 
 echo.
-echo ✅ Cambios subidos correctamente a GitHub.
+echo ✅ Cambios subidos a GitHub correctamente.
 pause
+
